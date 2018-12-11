aede9e78cd9fc73d34fd3eb0d19fc268d39ced0a
Jakub <43950128+JakubKazik@users.noreply.github.com>
2018-12-11 23:42:01 +0100

    Add files via upload

---
+++ Diagramy i przykładowy kod Jakub/GraczTest.java
@@ -0,0 +1,14 @@
+package chinskie.warcaby;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class GraczTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
---
+++ Diagramy i przykładowy kod Jakub/Obrazektest.java
@@ -0,0 +1,14 @@
+package chinskie.warcaby;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class Obrazektest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
