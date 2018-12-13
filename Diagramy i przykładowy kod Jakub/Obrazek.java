ffd8f41d70545af4eda79c8f1b9e90592c4bf0e9
Jakub <43950128+JakubKazik@users.noreply.github.com>
2018-12-13 01:44:57 +0100

    Add files via upload
    dodano rozmieszczenie wszystkich pionow

--- Diagramy i przykładowy kod Jakub/Obrazek.java
+++ Diagramy i przykładowy kod Jakub/Obrazek.java
@@ -48,28 +48,286 @@ public class Obrazek extends JPanel  implements MouseListener,  MouseMotionListe
 		Dimension dimension = new Dimension(image.getWidth(), image.getHeight());
 		setPreferredSize(dimension);
 		Ustaw_pionki();
+		Ustaw_pionki2();
+		Ustaw_pionki3();
+		Ustaw_pionki4();
+		Ustaw_pionki5();
+		Ustaw_pionki6();
 	}
 
 	public void Ustaw_pionki()//przemysl w ktorym miejscu wywolac metode, raczej nie paint comoponent
-	{
-		int ptkX;//doodamy przycisk w gui pt"rozmiesc pionki"
+	{//lewy gorny od niego sie zaczelo
+		int ptkX,ptkX2,ptkX3,ptkX4;//doodamy przycisk w gui pt"rozmiesc pionki"
 		//bedzie zapisywal punkty ale tylko raz i bedzie mozna je usuwac, usun met z mouse clicked
-		int ptkY;
+		int ptkY,ptkY2,ptkY3,ptkY4;
+		
+		
 		
 		ptkX=198;
 		ptkY=177;
+		ptkX2=214;
+		ptkY2=211;
+		ptkX3=235;
+		ptkY3=245;
+		ptkX4=256;
+		ptkY4=279;
 		
-		for(int i = 0; i<5; i++)
+		
+		for(int i = 0; i<4; i++)
 		{
 			punkty.add(new Point(ptkX,ptkY));
 			ptkX=ptkX+40;
 			
 		}
 		
+		for(int i = 0; i<3; i++)
+		{
+
+		 
+			punkty.add(new Point(ptkX2,ptkY2));
+			ptkX2=ptkX2+40;
+			
+		}
+		for(int i = 0; i<2; i++)
+		{
+
+		 
+			punkty.add(new Point(ptkX3,ptkY3));
+			ptkX3=ptkX3+40;
+			
+		}
 		
+		punkty.add(new Point(ptkX4,ptkY4));
 	}
 	
+	public void Ustaw_pionki2()//inny kolor bedzie 6 tych metod
+	{//lewy dolny operacje na y bazujac na void 1
+		int ptkX,ptkX2,ptkX3,ptkX4;
+
+		int ptkY,ptkY2,ptkY3,ptkY4;
+		
+		
+		
+		ptkX=198;
+		ptkY=453;
+		ptkX2=214;
+		ptkY2=419;
+		ptkX3=235;
+		ptkY3=385;
+		ptkX4=256;
+		ptkY4=351;
+		
+		
+		
+		for(int i = 0; i<4; i++)
+		{
+			punkty.add(new Point(ptkX,ptkY));
+			ptkX=ptkX+40;
+			
+		}
+		
+		for(int i = 0; i<3; i++)
+		{
+
+		 
+			punkty.add(new Point(ptkX2,ptkY2));
+			ptkX2=ptkX2+40;
+			
+		}
+		for(int i = 0; i<2; i++)
+		{
+
+		 
+			punkty.add(new Point(ptkX3,ptkY3));
+			ptkX3=ptkX3+40;
+			
+		}
+		
+		punkty.add(new Point(ptkX4,ptkY4));
+	}
+
+	public void Ustaw_pionki3()//inny kolor bedzie 6 tych metod
+	{//prawy dolny operacje na x bazujac na void 2
+		int ptkX,ptkX2,ptkX3,ptkX4;
+
+		int ptkY,ptkY2,ptkY3,ptkY4;
+		
+		
+		
+		ptkX=558;
+		ptkY=453;
+		ptkX2=574;
+		ptkY2=419;
+		ptkX3=595;
+		ptkY3=385;
+		ptkX4=616;
+		ptkY4=351;
+		
+		
+		
+		for(int i = 0; i<4; i++)
+		{
+			punkty.add(new Point(ptkX,ptkY));
+			ptkX=ptkX+40;
+			
+		}
+		
+		for(int i = 0; i<3; i++)
+		{
+
+		 
+			punkty.add(new Point(ptkX2,ptkY2));
+			ptkX2=ptkX2+40;
+			
+		}
+		for(int i = 0; i<2; i++)
+		{
+
+		 
+			punkty.add(new Point(ptkX3,ptkY3));
+			ptkX3=ptkX3+40;
+			
+		}
+		
+		punkty.add(new Point(ptkX4,ptkY4));
+	}
 	
+	public void Ustaw_pionki4()//inny kolor bedzie 6 tych metod
+	{//prawy gorny operacje na y bazujac na void 3
+		int ptkX,ptkX2,ptkX3,ptkX4;
+
+		int ptkY,ptkY2,ptkY3,ptkY4;
+		
+		
+		
+		ptkX=558;
+		ptkY=177;
+		ptkX2=574;
+		ptkY2=211;
+		ptkX3=595;
+		ptkY3=245;
+		ptkX4=616;
+		ptkY4=279;
+		
+		
+		
+		for(int i = 0; i<4; i++)
+		{
+			punkty.add(new Point(ptkX,ptkY));
+			ptkX=ptkX+40;
+			
+		}
+		
+		for(int i = 0; i<3; i++)
+		{
+
+		 
+			punkty.add(new Point(ptkX2,ptkY2));
+			ptkX2=ptkX2+40;
+			
+		}
+		for(int i = 0; i<2; i++)
+		{
+
+		 
+			punkty.add(new Point(ptkX3,ptkY3));
+			ptkX3=ptkX3+40;
+			
+		}
+		
+		punkty.add(new Point(ptkX4,ptkY4));
+	}
+	public void Ustaw_pionki5()//inny kolor bedzie 6 tych metod
+	{//dolny dolny operacje na nowej bazie
+		int ptkX,ptkX2,ptkX3,ptkX4;
+
+		int ptkY,ptkY2,ptkY3,ptkY4;
+		
+		
+		
+		ptkX=377;
+		ptkY=488;
+		ptkX2=393;
+		ptkY2=522;
+		ptkX3=414;
+		ptkY3=556;
+		ptkX4=435;
+		ptkY4=590;
+		
+		
+		
+		for(int i = 0; i<4; i++)
+		{
+			punkty.add(new Point(ptkX,ptkY));
+			ptkX=ptkX+40;
+			
+		}
+		
+		for(int i = 0; i<3; i++)
+		{
+
+		 
+			punkty.add(new Point(ptkX2,ptkY2));
+			ptkX2=ptkX2+40;
+			
+		}
+		for(int i = 0; i<2; i++)
+		{
+
+		 
+			punkty.add(new Point(ptkX3,ptkY3));
+			ptkX3=ptkX3+40;
+			
+		}
+		
+		punkty.add(new Point(ptkX4,ptkY4));
+	}
+	
+	public void Ustaw_pionki6()//inny kolor bedzie 6 tych metod
+	{//gorny gorny operacje na nowej bazie
+		int ptkX,ptkX2,ptkX3,ptkX4;
+
+		int ptkY,ptkY2,ptkY3,ptkY4;
+		
+		
+		
+		ptkX=377;
+		ptkY=142;
+		ptkX2=393;
+		ptkY2=108;
+		ptkX3=414;
+		ptkY3=70;
+		ptkX4=435;
+		ptkY4=36;
+		
+		
+		
+		for(int i = 0; i<4; i++)
+		{
+			punkty.add(new Point(ptkX,ptkY));
+			ptkX=ptkX+40;
+			
+		}
+		
+		for(int i = 0; i<3; i++)
+		{
+
+		 
+			punkty.add(new Point(ptkX2,ptkY2));
+			ptkX2=ptkX2+40;
+			
+		}
+		for(int i = 0; i<2; i++)
+		{
+
+		 
+			punkty.add(new Point(ptkX3,ptkY3));
+			ptkX3=ptkX3+40;
+			
+		}
+		
+		punkty.add(new Point(ptkX4,ptkY4));
+	}
 	
 	//@Override
 	public void paintComponent(Graphics g) {
