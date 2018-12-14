77993397081f4938ad1e7e72693fc7d817415f08
Jakub <43950128+JakubKazik@users.noreply.github.com>
2018-12-14 03:23:06 +0100

    Add files via upload
    Wstepne klasy testowe

---
+++ Diagramy i przykładowy kod Jakub/PlanszaTest.java
@@ -0,0 +1,14 @@
+package chinskie.warcaby;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class PlanszaTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
---
+++ Diagramy i przykładowy kod Jakub/RamkaPlanszyTest.java
@@ -0,0 +1,14 @@
+package chinskie.warcaby;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class RamkaPlanszyTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
