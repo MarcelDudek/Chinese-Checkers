fbbdefb80310f45f07034ab5f209ce3312af19fc
Jakub <43950128+JakubKazik@users.noreply.github.com>
2018-12-14 03:20:14 +0100

    Add files via upload
    Klasy odpowiedzialne, za pionki oraz ich rozmiesczenie na planszy oraz ich kolory, Za wyglad planszy oraz poruszanie sie pionow po niej, oraz za stworzenie ramki, w ktorej plansza sie znajduje i odpalenie planszy

---
+++ Diagramy i przykładowy kod Jakub/Piony.java
@@ -0,0 +1,285 @@
+package chinskie.warcaby;
+
+import java.awt.Color;
+import java.awt.Graphics2D;
+import java.awt.Point;
+import java.util.ArrayList;
+
+public class Piony {
+
+	ArrayList<Point> punkty = new ArrayList<Point>();
+
+	public void Ustaw_pionki(Graphics2D g3d)
+
+	{
+		int ptkX, ptkX2, ptkX3, ptkX4;
+		int ptkY, ptkY2, ptkY3, ptkY4;
+		
+		g3d.setColor(Color.BLACK);
+		
+		ptkX = 198;
+		ptkY = 177;
+		ptkX2 = 214;
+		ptkY2 = 211;
+		ptkX3 = 235;
+		ptkY3 = 245;
+		ptkX4 = 256;
+		ptkY4 = 279;
+
+		for (int i = 0; i < 4; i++) {
+			punkty.add(new Point(ptkX, ptkY));
+			ptkX = ptkX + 40;
+
+		}
+
+		for (int i = 0; i < 3; i++) {
+
+			punkty.add(new Point(ptkX2, ptkY2));
+			ptkX2 = ptkX2 + 40;
+
+		}
+		for (int i = 0; i < 2; i++) {
+
+			punkty.add(new Point(ptkX3, ptkY3));
+			ptkX3 = ptkX3 + 40;
+
+		}
+
+		punkty.add(new Point(ptkX4, ptkY4));
+
+		int x1, y1;
+
+		for (int i = 0; i < 10; i++) {
+			x1 = (int) punkty.get(i).getX();
+			y1 = (int) punkty.get(i).getY();
+			g3d.fillOval(x1, y1, 25, 25);
+		}
+
+	}
+
+	public void Ustaw_pionki2(Graphics2D g3d) {// lewy dolny
+		int ptkX, ptkX2, ptkX3, ptkX4;
+		int ptkY, ptkY2, ptkY3, ptkY4;
+
+		g3d.setColor(Color.CYAN);
+		
+		ptkX = 198;
+		ptkY = 453;
+		ptkX2 = 214;
+		ptkY2 = 419;
+		ptkX3 = 235;
+		ptkY3 = 385;
+		ptkX4 = 256;
+		ptkY4 = 351;
+
+		for (int i = 0; i < 4; i++) {
+			punkty.add(new Point(ptkX, ptkY));
+			ptkX = ptkX + 40;
+
+		}
+
+		for (int i = 0; i < 3; i++) {
+
+			punkty.add(new Point(ptkX2, ptkY2));
+			ptkX2 = ptkX2 + 40;
+
+		}
+		for (int i = 0; i < 2; i++) {
+
+			punkty.add(new Point(ptkX3, ptkY3));
+			ptkX3 = ptkX3 + 40;
+
+		}
+
+		punkty.add(new Point(ptkX4, ptkY4));
+
+		int x1, y1;
+
+		for (int i = 10; i < 20; i++) {
+			x1 = (int) punkty.get(i).getX();
+			y1 = (int) punkty.get(i).getY();
+			g3d.fillOval(x1, y1, 25, 25);
+		}
+	}
+
+	public void Ustaw_pionki3(Graphics2D g3d) {// prawy dolny
+		int ptkX, ptkX2, ptkX3, ptkX4;
+		int ptkY, ptkY2, ptkY3, ptkY4;
+
+		g3d.setColor(Color.BLUE);
+		
+		ptkX = 558;
+		ptkY = 453;
+		ptkX2 = 574;
+		ptkY2 = 419;
+		ptkX3 = 595;
+		ptkY3 = 385;
+		ptkX4 = 616;
+		ptkY4 = 351;
+
+		for (int i = 0; i < 4; i++) {
+			punkty.add(new Point(ptkX, ptkY));
+			ptkX = ptkX + 40;
+
+		}
+
+		for (int i = 0; i < 3; i++) {
+
+			punkty.add(new Point(ptkX2, ptkY2));
+			ptkX2 = ptkX2 + 40;
+
+		}
+		for (int i = 0; i < 2; i++) {
+
+			punkty.add(new Point(ptkX3, ptkY3));
+			ptkX3 = ptkX3 + 40;
+
+		}
+
+		punkty.add(new Point(ptkX4, ptkY4));
+
+		int x1, y1;
+
+		for (int i = 20; i < 30; i++) {
+			x1 = (int) punkty.get(i).getX();
+			y1 = (int) punkty.get(i).getY();
+			g3d.fillOval(x1, y1, 25, 25);
+		}
+	}
+
+	public void Ustaw_pionki4(Graphics2D g3d) {// prawy gorny
+		int ptkX, ptkX2, ptkX3, ptkX4;
+		int ptkY, ptkY2, ptkY3, ptkY4;
+
+		g3d.setColor(Color.GRAY);
+		
+		ptkX = 558;
+		ptkY = 177;
+		ptkX2 = 574;
+		ptkY2 = 211;
+		ptkX3 = 595;
+		ptkY3 = 245;
+		ptkX4 = 616;
+		ptkY4 = 279;
+
+		for (int i = 0; i < 4; i++) {
+			punkty.add(new Point(ptkX, ptkY));
+			ptkX = ptkX + 40;
+
+		}
+
+		for (int i = 0; i < 3; i++) {
+
+			punkty.add(new Point(ptkX2, ptkY2));
+			ptkX2 = ptkX2 + 40;
+
+		}
+		for (int i = 0; i < 2; i++) {
+
+			punkty.add(new Point(ptkX3, ptkY3));
+			ptkX3 = ptkX3 + 40;
+
+		}
+
+		punkty.add(new Point(ptkX4, ptkY4));
+
+		int x1, y1;
+
+		for (int i = 30; i < 40; i++) {
+			x1 = (int) punkty.get(i).getX();
+			y1 = (int) punkty.get(i).getY();
+			g3d.fillOval(x1, y1, 25, 25);
+		}
+	}
+	public void Ustaw_pionki5(Graphics2D g3d) {// dolny dolny
+		int ptkX, ptkX2, ptkX3, ptkX4;
+		int ptkY, ptkY2, ptkY3, ptkY4;
+
+		g3d.setColor(Color.DARK_GRAY);
+		
+		ptkX = 377;
+		ptkY = 488;
+		ptkX2 = 393;
+		ptkY2 = 522;
+		ptkX3 = 414;
+		ptkY3 = 556;
+		ptkX4 = 435;
+		ptkY4 = 590;
+
+		for (int i = 0; i < 4; i++) {
+			punkty.add(new Point(ptkX, ptkY));
+			ptkX = ptkX + 40;
+
+		}
+
+		for (int i = 0; i < 3; i++) {
+
+			punkty.add(new Point(ptkX2, ptkY2));
+			ptkX2 = ptkX2 + 40;
+
+		}
+		for (int i = 0; i < 2; i++) {
+
+			punkty.add(new Point(ptkX3, ptkY3));
+			ptkX3 = ptkX3 + 40;
+
+		}
+
+		punkty.add(new Point(ptkX4, ptkY4));
+
+		int x1, y1;
+
+		for (int i = 40; i < 50; i++) {
+			x1 = (int) punkty.get(i).getX();
+			y1 = (int) punkty.get(i).getY();
+			g3d.fillOval(x1, y1, 25, 25);
+		}
+	}
+
+	public void Ustaw_pionki6(Graphics2D g3d) {// gorny gorny
+		int ptkX, ptkX2, ptkX3, ptkX4;
+		int ptkY, ptkY2, ptkY3, ptkY4;
+
+		g3d.setColor(Color.MAGENTA);
+		
+		ptkX = 377;
+		ptkY = 142;
+		ptkX2 = 393;
+		ptkY2 = 108;
+		ptkX3 = 414;
+		ptkY3 = 70;
+		ptkX4 = 435;
+		ptkY4 = 36;
+
+		for (int i = 0; i < 4; i++) {
+			punkty.add(new Point(ptkX, ptkY));
+			ptkX = ptkX + 40;
+
+		}
+
+		for (int i = 0; i < 3; i++) {
+
+			punkty.add(new Point(ptkX2, ptkY2));
+			ptkX2 = ptkX2 + 40;
+
+		}
+		for (int i = 0; i < 2; i++) {
+
+			punkty.add(new Point(ptkX3, ptkY3));
+			ptkX3 = ptkX3 + 40;
+
+		}
+
+		punkty.add(new Point(ptkX4, ptkY4));
+
+		int x1, y1;
+
+		for (int i = 50; i < 60; i++) {
+			x1 = (int) punkty.get(i).getX();
+			y1 = (int) punkty.get(i).getY();
+			g3d.fillOval(x1, y1, 25, 25);
+
+		}
+
+	}
+}
---
+++ Diagramy i przykładowy kod Jakub/Plansza.java
@@ -0,0 +1,123 @@
+package chinskie.warcaby;
+
+import java.awt.*;
+import java.awt.event.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+
+import javax.imageio.ImageIO;
+import javax.swing.JPanel;
+
+public class Plansza extends JPanel
+	implements
+MouseListener,
+MouseMotionListener {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	Point movingPoint = null;
+	ArrayList<Point> punkty = new ArrayList<Point>();
+
+	private BufferedImage image;
+
+	Piony pionki;
+
+	int Xsrodek;
+	int Ysrodek;
+
+	public Plansza() {
+		super();
+
+		addMouseListener(this);
+		addMouseMotionListener(this);
+		setPreferredSize(new Dimension(300, 400));
+
+		File imageFile = new File(
+				"C:\\Users\\Kazik\\Desktop\\warcaby2Obraz.jpg");
+		try {
+			image = ImageIO.read(imageFile);
+		} catch (IOException e) {
+			System.err.println("Blad odczytu obrazka");
+			e.printStackTrace();
+		}
+
+		Dimension dimension = new Dimension(image.getWidth(),
+				image.getHeight());
+		setPreferredSize(dimension);
+		pionki = new Piony();
+
+	}
+
+	public void paintComponent(Graphics g) {
+		Graphics2D g2d = (Graphics2D) g;
+		g2d.drawImage(image, 0, 0, this);
+		for (Point p : pionki.punkty) {
+			punkty.add(p);
+		}
+		pionki.Ustaw_pionki(g2d);
+
+	}
+
+	public void mouseClicked(MouseEvent e) {
+		;
+	}
+	public void mouseEntered(MouseEvent e) {
+		;
+	}
+
+	public void mouseExited(MouseEvent e) {
+		;
+	}
+
+	public void mousePressed(MouseEvent e) {
+		int x = 0;
+		int y = 0;
+		x = e.getX();
+		y = e.getY();
+		int x2, y2;
+
+		if (e.getButton() != MouseEvent.BUTTON3) {
+
+			int index = 0;
+			int size = punkty.size();
+			Point p;
+			while (movingPoint == null && index < size) {
+				p = punkty.get(index);
+				x2 = (int) p.getX();
+				y2 = (int) p.getY();
+				Xsrodek = e.getX() - x2;
+				Ysrodek = e.getY() - y2;
+
+				if (x >= x2 && y >= y2 && x <= x2 + 25 && y <= y2 + 25) {
+					movingPoint = p;
+				}
+				index++;
+
+			}
+		}
+
+		repaint();
+	}
+
+	public void mouseReleased(MouseEvent e) {
+
+		movingPoint = null;
+
+	}
+	public void mouseDragged(MouseEvent e) {
+		if (movingPoint != null) {
+
+			movingPoint.x = e.getX() - Xsrodek;
+			movingPoint.y = e.getY() - Ysrodek;
+			repaint();
+		}
+	}
+	public void mouseMoved(MouseEvent e) {
+		;
+	}
+
+}
