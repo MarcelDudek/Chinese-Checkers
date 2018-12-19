9f0c554ecc487fa6b8ccf499410d750f49197ba1
Marcel Dudek <marcel.k.dudek@gmail.com>
2018-12-19 10:25:19 +0100

    Usprzątniety trochę kod serwera.

--- Chinese-Checkers/src/main/java/tp/chinesecheckers/serwer/Serwer.java
+++ Chinese-Checkers/src/main/java/tp/chinesecheckers/serwer/Serwer.java
@@ -12,126 +12,152 @@ import java.util.Random;
 
 import tp.chinesecheckers.GraDomyslnaSerwer;
 import tp.chinesecheckers.TworcaGryDomyslnejSerwer;
+import tp.chinesecheckers.exception.NiepoprawnyRuch;
 
 public class Serwer implements Runnable{
 
   private int port;
   private int iloscGraczy;
   private boolean uzupelnijBotami;
-  private boolean zakonczGre;
   private List<Polaczenie> polaczenie = new ArrayList<Polaczenie>();
   private String wiadomoscOdPolaczenia = "";
+  private GraDomyslnaSerwer gra;
+  private ServerSocket socketSerwer;
   
   public Serwer(int port, int iloscGraczy) {
     this.port = port;
     this.iloscGraczy = iloscGraczy;
     this.uzupelnijBotami = false;
-    this.zakonczGre = false;
+    this.gra = null;
+    this.socketSerwer = null;
   }
   
   public void uzupelnijGreBotami() {
     this.uzupelnijBotami = true;
   }
   
-  public void wymusZakonczenieGry() {
-    this.zakonczGre = true;
+  public void run() {
+    try {
+      socketSerwer = new ServerSocket(port);
+    } catch(IOException ex) {
+      ex.printStackTrace();
+      return;
+    }
+    
+    System.out.println("Uruchomiono serwer");
+    
+    //Oczekiwanie na dolaczenie graczy
+    while(!uzupelnijBotami) {
+      polaczZKlientem();
+      
+      //Jezeli osiagnieto wymagana liczbe graczy
+      if(polaczenie.size() == iloscGraczy) {
+        break;
+      }
+    }
+    
+    //Start gry
+    System.out.println("Startowanie gry");
+    
+    //Stworzenie obiektu gry
+    TworcaGryDomyslnejSerwer tworca = new TworcaGryDomyslnejSerwer(iloscGraczy);
+    for (int i = 0; i < iloscGraczy; i++) {
+      if (i >= polaczenie.size()) {
+        tworca.dodajGracza("Bot" + i, 0, true);
+        continue;
+      }
+      Polaczenie pol = polaczenie.get(i);
+      tworca.dodajGracza(pol.podajNazwe(), 0, false);
+    }
+    gra = (GraDomyslnaSerwer)tworca.stworzGre();
+    
+    //Wylosowanie startujacego gracza
+    Random rand = new Random();
+    int aktualnyRuch = rand.nextInt(iloscGraczy);
+    
+    //Petla gry
+    while(true) {
+      rozegrajRunde(aktualnyRuch);
+      
+      //Przygotowanie do nastepnej rundy
+      wiadomoscOdPolaczenia = "";
+      aktualnyRuch++;
+      if(aktualnyRuch ==  iloscGraczy) {
+        aktualnyRuch = 0;
+      }
+    }
+    //socketSerwer.close();
   }
   
-  public void run() {
-    String wiadomosc;
-    
-    System.out.println("Uruchomiono");
-    
+  private void polaczZKlientem() {
     try {
-      ServerSocket socketSerwer = new ServerSocket(port);
-      //Oczekiwanie na dolaczenie graczy
-      while(!uzupelnijBotami) {
-        Socket socketKlient = socketSerwer.accept();
-        System.out.println("Polaczono klienta");
-        InputStreamReader reader = new InputStreamReader(socketKlient.getInputStream());
-        BufferedReader readerBuffer = new BufferedReader(reader);
-        PrintWriter writer = new PrintWriter(socketKlient.getOutputStream());
-        
-        //Pobierz nazwe
-        writer.println("podaj_nazwe");
+      //Czekaj na polaczenie klienta z serwerem
+      Socket socketKlient = socketSerwer.accept();
+      System.out.println("Polaczono klienta");
+      InputStreamReader reader = new InputStreamReader(socketKlient.getInputStream());
+      BufferedReader readerBuffer = new BufferedReader(reader);
+      PrintWriter writer = new PrintWriter(socketKlient.getOutputStream());
+      
+      //Pobierz nazwe
+      writer.println("podaj_nazwe");
+      writer.flush();
+      String wiadomosc = readerBuffer.readLine();
+      
+      //Sprawdz czy nazwa zajeta
+      if(sprawdzCzyNazwaWolna(wiadomosc)) {
+        //Dodanie klienta do obslugiwanych polaczen i wyslanie potwierdzienia poprawnego polaczenia
+        polaczenie.add(new Polaczenie(wiadomosc, socketKlient, this));
+        System.out.println("Polaczono gracza: " + wiadomosc);
+        writer.println("polaczenie_udane");
         writer.flush();
-        wiadomosc = readerBuffer.readLine();
         
-        //Sprawdz czy nazwa zajeta
-        if(sprawdzCzyNazwaWolna(wiadomosc)) {
-          polaczenie.add(new Polaczenie(wiadomosc, socketKlient, this));
-          System.out.println("Polaczono gracza: " + wiadomosc);
-          writer.println("polaczenie_udane");
-          writer.flush();
-          
-          Thread thr = new Thread(polaczenie.get(polaczenie.size() - 1));
-          thr.start();
-        } else {
-          System.out.println("Nazwa zajeta, rozlaczono klienta.");
-          writer.println("nazwa_zajeta");
-          writer.flush();
-          socketKlient.close();
-        }
-        
-        //Jezeli osiagnieto wymagana liczbe graczy
-        if(polaczenie.size() == iloscGraczy) {
-          break;
-        }
+        //Uruchomienie watku obslugujacego polaczenie z danym klientem
+        Thread thr = new Thread(polaczenie.get(polaczenie.size() - 1));
+        thr.start();
+      } else {
+        //Nieudane polaczenie, wyslij do klienta informacje o zajetej nazwie i rozlacz polaczenie
+        System.out.println("Nazwa zajeta, rozlaczono klienta.");
+        writer.println("nazwa_zajeta");
+        writer.flush();
+        socketKlient.close();
       }
-      
-      //Start gry
-      System.out.println("Startowanie gry");
-      
-      //Stworzenie obiektu gry
-      TworcaGryDomyslnejSerwer tworca = new TworcaGryDomyslnejSerwer(iloscGraczy);
-      for (int i = 0; i < iloscGraczy; i++) {
-        if (i >= polaczenie.size()) {
-          tworca.dodajGracza("Bot" + i, 0, true);
-          continue;
-        }
-        Polaczenie pol = polaczenie.get(i);
-        tworca.dodajGracza(pol.podajNazwe(), 0, false);
-      }
-      GraDomyslnaSerwer gra = (GraDomyslnaSerwer)tworca.stworzGre();
-      
-      //Wylosowanie startujacego gracza
-      Random rand = new Random();
-      int aktualnyRuch = 1;//rand.nextInt(iloscGraczy);
-      
-      //Petla gry
-      while(true) {
-        //Podbicie licznika rund
-        gra.ustawRunde(gra.podajRunde() + 1);
-        
-        //wyslanie stanu gry
-        String wiadomoscStanuGry = gra.wygenerujWiadomosc(polaczenie.get(aktualnyRuch).podajNazwe());
-        for (int i = 0; i < polaczenie.size(); i++) {
-          polaczenie.get(i).wyslijWiadomosc(wiadomoscStanuGry);
-        }
-        
-        Polaczenie polRuch = polaczenie.get(aktualnyRuch);
-        polRuch.ruch = true;
-        while(wiadomoscOdPolaczenia.equals("")) {
-          Thread.sleep(1000);
-        }
-        
-        //Rozkodowanie wiadomosci
-        String[] pozycja = wiadomoscOdPolaczenia.split(",");
-        
-        gra.wykonajRuch(polRuch.podajNazwe(), Integer.parseInt(pozycja[0]), Integer.parseInt(pozycja[1]),
-            Integer.parseInt(pozycja[2]), Integer.parseInt(pozycja[3]));
-        
-        polRuch.ruch = false;
-        
-        aktualnyRuch++;
-        if(aktualnyRuch ==  iloscGraczy) {
-          aktualnyRuch = 0;
-        }
-      }
-      
-    } catch(Exception ex) {
+    } catch (IOException ex) {
       ex.printStackTrace();
     }
+  }
+  
+  private void rozegrajRunde(int aktualnyRuch) {
+    //Podbicie licznika rund
+    gra.ustawRunde(gra.podajRunde() + 1);
+    
+    //wyslanie stanu gry
+    String wiadomoscStanuGry = gra.wygenerujWiadomosc(polaczenie.get(aktualnyRuch).podajNazwe());
+    for (int i = 0; i < polaczenie.size(); i++) {
+      polaczenie.get(i).wyslijWiadomosc(wiadomoscStanuGry);
+    }
+    
+    //Czekanie na wiadomosc od klienta, ktory wykonuje aktualny ruch
+    Polaczenie polRuch = polaczenie.get(aktualnyRuch);
+    polRuch.ruch = true;
+    try {
+      while(wiadomoscOdPolaczenia.equals("")) {
+        Thread.sleep(1000);
+      }
+    } catch (InterruptedException ex) {
+      ex.printStackTrace();
+    }
+    
+    //Rozkodowanie wiadomosci
+    String[] pozycja = wiadomoscOdPolaczenia.split(",");
+    
+    try {
+      gra.wykonajRuch(polRuch.podajNazwe(), Integer.parseInt(pozycja[0]), Integer.parseInt(pozycja[1]),
+          Integer.parseInt(pozycja[2]), Integer.parseInt(pozycja[3]));
+    } catch (NiepoprawnyRuch ex) {
+      System.err.println("Niepoprawny ruch.");
+    }
+    
+    polRuch.ruch = false;
   }
   
   private boolean sprawdzCzyNazwaWolna(String nazwa) {
